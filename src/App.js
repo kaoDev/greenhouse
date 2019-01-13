3d1e2f7acd178885c83965390a849f1d81cdd33a
Kalle Ott <kalle.ott@cap3.de>
2019-01-13 18:58:11 +0100

    ui with firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import FirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { AppContent } from './AppContent'
 
-class App extends Component {
+// Configure Firebase.
+const config = {
+  apiKey: 'AIzaSyBvRhAYjqwvMYG9P4h0CgM3fg_TMYpqwDM',
+  authDomain: 'iot-2018-65259.firebaseapp.com',
+  databaseURL: 'https://iot-2018-65259.firebaseio.com',
+  projectId: 'iot-2018-65259'
+}
+const firebaseApp = firebase.initializeApp(config)
+
+const firestore = firebaseApp.firestore()
+firestore.settings({ timestampsInSnapshots: true })
+const auth = firebase.auth()
+
+export default class App extends React.Component {
+  state = { user: null }
+
+  componentDidMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged(
+      user => {
+        this.setState({ user })
+      },
+      error => {
+        console.error(error)
+        this.setState({ user: null })
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe()
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <h1>My Greenhouse</h1>
         </header>
+        {this.state.user ? (
+          <AppContent firestore={firestore} auth={auth} />
+        ) : (
+          <FirebaseAuth
+            uiConfig={{
+              signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
+            }}
+            firebaseAuth={auth}
+          />
+        )}
       </div>
-    );
+    )
   }
 }
-
-export default App;
